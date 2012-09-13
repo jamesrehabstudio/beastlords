a454c488ceff953ade911ef59d08b99fdc3fbc5c
James <james@rehabstudio.com>
2012-09-13 10:31:47 +0100

    Map now uses .map files rather than a database.
    Signed-off-by: James <james@rehabstudio.com>

--- save.php
+++ save.php
@@ -12,38 +12,26 @@ header('Cache-Control: no-cache, must-revalidate');
 header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
 header('Content-type: application/json');
 
-$id = isset( $_REQUEST['id'] ) ? (int)$_REQUEST['id'] : false;
 $name = isset( $_REQUEST['name'] ) ? stripslashes( $_REQUEST['name'] ) : false;
 
 if ( !empty( $_POST ) ) {
 	$name = addslashes( $_POST['name'] );
 	$data = addslashes( $_POST['data'] );
 
-	$map = false;
-	$sql = mysql_query("SELECT * FROM maps WHERE name = '$name'");
-	while( $row = mysql_fetch_assoc( $sql ) ){$map = $row;};
-
-	if ( $map ) {
-		//Update existing map
-		mysql_query( "UPDATE maps SET name = '$name', data = '$data' WHERE name='$name'" );
-	} else {
-		//Create a new map
-		mysql_query( "INSERT INTO maps (name, data) VALUES ('$name', '$data')" );
-		$id = mysql_insert_id();
-	}
+	//$map = false;
+	//$sql = mysql_query("SELECT * FROM maps WHERE name = '$name'");
+	//while( $row = mysql_fetch_assoc( $sql ) ){$map = $row;};
+	
+	$filename = "maps/$name.map";
+	$input = fopen( $filename, 'w' );
+	fwrite( $input, $data );
+	fclose( $input );
 }
-if ( $id ) {
-	$map = false;
-	$sql = mysql_query("SELECT * FROM maps WHERE id = '$id'");
-	while( $row = mysql_fetch_assoc( $sql ) ){$map = $row;};
-	
-	echo $map['data'];
-} elseif ( $name ) {
-	$map = false;
-	$sql = mysql_query("SELECT * FROM maps WHERE name = '$name'");
-	while( $row = mysql_fetch_assoc( $sql ) ){$map = $row;};
-	
-	echo $map['data'];
+
+if ( $name ) {
+	$filename = "maps/$name.map";
+	$output = fopen( $filename, 'r' );
+	echo stripslashes( fread( $output, filesize($filename) ) );
 } else {
 	echo json_encode( Array( "message" => "No data", "name" => $name ) );
 }
