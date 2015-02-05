62ff5e5646d3a5533cd3df072fd86145e25f95db
james <james@rehabstudio.com>
2015-02-05 15:48:01 +0000

    Oriax

--- js/platformer.js
+++ js/platformer.js
@@ -45,6 +45,7 @@ function Player(x, y){
 	this.attack_withdraw = 5.0;
 	
 	this.on("death", function(){
+		game.slow(0,20.0);
 		this.destroy();
 	});
 	this.on("struck", function(obj,pos,damage){
@@ -62,7 +63,11 @@ function Player(x, y){
 			this.trigger("hurt",obj,damage);
 		}
 	});
-	
+	this.on("hurt", function(obj, damage){
+		if( this.invincible <= 0 ) {
+			game.slow(0,5.0);
+		}
+	})
 	this._weapontimeout = 0;
 	this.addModule( mod_rigidbody );
 	this.addModule( mod_camera );
@@ -222,12 +227,13 @@ function Knight(x,y){
 	
 	this.life = 40;
 	this.damage = 15;
+	this.collideDamage = 10;
 	this.mass = 1.5;
 	this.inviciple_time = this.hurt_time;
 	
 	this.on("collideObject", function(obj){
 		if( this.team == obj.team ) return;
-		obj.trigger("hurt", this, 5 );
+		obj.trigger("hurt", this, this.collideDamage );
 	});
 	this.on("struck", function(obj,pos,damage){
 		if( this.team == obj.team ) return;
@@ -246,7 +252,7 @@ function Knight(x,y){
 	});
 	this.on("hurt", function(){
 		this.states.attack = -1.0;
-		this.states.cooldown = 40.0;
+		this.states.cooldown = Math.random() > 0.6 ? 10 : 40;
 		this.states.guard = Math.random() > 0.5 ? 1 : 2;
 	});
 	this.on("death", function(){
@@ -357,11 +363,12 @@ function Skeleton(x,y){
 	this.life = 20;
 	this.mass = 0.8;
 	this.damage = 15;
+	this.collideDamage = 8;
 	this.inviciple_tile = this.hurt_time;
 	
 	this.on("collideObject", function(obj){
 		if( this.team == obj.team ) return;
-		obj.trigger("hurt", this, 5 );
+		obj.trigger("hurt", this, this.collideDamage );
 	});
 	this.on("collideHorizontal", function(x){
 		this.states.prep_jump = true;
@@ -476,11 +483,12 @@ function Marquis(x,y){
 	this.life = 80;
 	this.mass = 2.0;
 	this.damage = 25;
+	this.collideDamage = 10;
 	this.inviciple_tile = this.hurt_time;
 	
 	this.on("collideObject", function(obj){
 		if( this.team == obj.team ) return;
-		obj.trigger("hurt", this, 5 );
+		obj.trigger("hurt", this, this.collideDamage );
 	});
 	this.on("struck", function(obj,pos,damage){
 		if( this.team == obj.team ) return;
@@ -564,12 +572,15 @@ function Amon(x,y){
 	this.width = 16;
 	this.height = 16;
 	
-	this.speed = 3.0;
+	this.speed = 2.5;
 	this.sprite = sprites.amon;
 	
 	this.addModule( mod_rigidbody );
 	this.addModule( mod_combat );
 	
+	this.on("struck", function(obj,pos,damage){
+		this.trigger("hurt", obj, this.damage );
+	});
 	this.on("collideObject", function(obj){
 		if( this.team == obj.team ) return;
 		obj.trigger("hurt", this, this.damage );
@@ -580,16 +591,185 @@ function Amon(x,y){
 	this.on("collideVertical", function(dir){
 		this.force.y *= -1;
 	});
+	this.on("death", function(obj,pos,damage){
+		this.destroy();
+	});
 	
+	this.life = 30;
 	this.collisionReduction = 1.0;
 	this.friction = 0.0;
-	this.force.x = this.force.y = this.speed;
+	this.hurt_time = 30.0;
+	this.invincible_time = 30.0;
+	this.force.x = this.speed * (Math.random() > 0.5 ? -1 : 1);
+	this.force.y = this.speed * (Math.random() > 0.5 ? -1 : 1);
+	this.backupForce = new Point(this.force.x, this.force.y);
+	
 	this.mass = 0;
 	this.gravity = 0.0;
 }
 Amon.prototype.update = function(){
 	this.frame = (this.frame + this.delta * 0.2) % 2;
-	this.flip = this.force.x < 0;
+	if( this.hurt < 0 ) {
+		if( Math.abs( this.force.x ) > 0.1 ) {
+			this.backupForce = new Point(this.force.x, this.force.y);
+		} else {
+			this.force = new Point(this.backupForce.x, this.backupForce.y);
+		}
+		this.flip = this.force.x < 0;
+	} else {
+		this.force.x = this.force.y = 0;
+	}
+}
+
+Oriax.prototype = new GameObject();
+Oriax.prototype.constructor = GameObject;
+function Oriax(x,y){
+	this.constructor();
+	this.position.x = x;
+	this.position.y = y;
+	this.width = 16;
+	this.height = 32;
+	
+	this.speed = 0.1;
+	this.sprite = sprites.oriax;
+	
+	this.addModule( mod_rigidbody );
+	this.addModule( mod_combat );
+	
+	this.on("struck", function(obj,pos,damage){
+		this.trigger("hurt", obj, this.damage );
+	});
+	this.on("hurt", function(obj,damage){
+		this.states.attack = 0;
+	});
+	this.on("collideObject", function(obj){
+		if( this.team == obj.team ) return;
+		obj.trigger("hurt", this, this.collideDamage );
+	});
+	this.on("collideHorizontal", function(dir){
+		this.states.backup = !this.states.backup;
+	});
+	this.on("death", function(obj,pos,damage){
+		this.destroy();
+	});
+	
+	this.life = 40;
+	this.collideDamage = 5;
+	this.mass = 1.0;
+	
+	this.states = {
+		"cooldown" : 50,
+		"attack" : 0,
+		"thrown" : false,
+		"backup" : false
+	};
+	this.attack = {
+		"warm" : 45,
+		"release" : 25
+	};
+}
+Oriax.prototype.update = function(){
+	var dir = this.position.subtract(_player.position);
+	if( this.hurt < 0 ) {
+		if( this.states.attack < 0 ){
+			var direction = (this.flip ? -1 : 1) * (this.states.backup ? -1 : 1);
+			this.force.x += this.speed * this.delta * direction;
+		}
+		this.flip = dir.x > 0;
+		if( Math.abs(dir.x) < 32 ) this.states.backup = true;
+		if( Math.abs(dir.x) > 104 ) this.states.backup = false;
+		
+		if( this.states.cooldown < 0 ){
+			this.states.attack = this.attack.warm;
+			this.states.cooldown = 60;
+		}
+		
+		if( this.states.attack > 0 ){
+			if( this.states.attack < this.attack.release && !this.states.thrown ){
+				this.states.thrown = true;
+				var missle = new SnakeBullet(this.position.x, this.position.y-8, (this.flip?-1:1) );
+				game.addObject( missle ); 
+			}
+		} else {
+			this.states.thrown = false;
+		}
+		
+		this.states.cooldown -= this.delta;
+		this.states.attack -= this.delta;
+	}
+	
+	/* Animate */
+	if( this.hurt > 0 ) {
+		this.frame = (this.frame + 1) % 2;
+		this.frame_row = 2;
+	} else {
+		if( this.states.attack > 0 ) {
+			this.frame = this.states.attack > this.attack.release ? 0 : 1;
+			this.frame_row = 1;
+		} else {
+			this.frame = (this.frame + this.delta * Math.abs(this.force.x) * 0.3 ) % 2
+			this.frame_row = 0;
+		}
+	}
+}
+
+SnakeBullet.prototype = new GameObject();
+SnakeBullet.prototype.constructor = GameObject;
+function SnakeBullet(x,y,d){
+	this.constructor();
+	this.position.x = x;
+	this.position.y = y;
+	this.width = 16;
+	this.height = 16;
+	this.origin.y = 0.8;
+	
+	this.speed = 0.2;
+	this.sprite = sprites.oriax;
+	
+	this.addModule( mod_rigidbody );
+	this.addModule( mod_combat );
+	
+	this.on("struck", function(obj,pos,damage){
+		this.trigger("hurt", obj, this.damage );
+	});
+	this.on("collideObject", function(obj){
+		if( this.team != obj.team ) {
+			obj.trigger("struck", this, this.position, this.collideDamage );
+			this.trigger("death");
+		} else if( this.states.landed ){
+			this.trigger("death");
+		}
+	});
+	this.on("collideVertical", function(dir){
+		if( !this.states.landed ){
+			this.states.landed = true;
+			this.flip = !this.flip;
+		}
+	});
+	this.on("death", function(obj,pos,damage){
+		this.destroy();
+	});
+	this.flip = d < 0;
+	this.force.x = d * 8;
+	this.life = 3;
+	this.collideDamage = 5;
+	this.mass = 0.0;
+	this.gravity = 0.1;
+	
+	this.states = {
+		"landed" : false
+	}
+}
+SnakeBullet.prototype.update = function(){
+	this.frame = Math.max( (this.frame + this.delta * 0.2) % 4, 2);
+	this.frame_row = 0;
+	this.friction = this.grounded ? 0.2 : 0.05;
+	
+	if( this.hurt < 0 && this.states.landed) {
+		this.gravity = 1.0;
+		var direction = (this.flip ? -1 : 1);
+		this.force.x += this.speed * this.delta * direction;
+	}
 }
 
 Lift.prototype = new GameObject();
@@ -692,6 +872,7 @@ function Item(x,y){
 				//collectable
 				if( obj.items.indexOf( this ) < 0 ) {
 					obj.items.push( this );
+					game.slow(0,10.0);
 					this.destroy();
 				}
 			}
@@ -858,6 +1039,7 @@ var mod_combat = {
 		this.invincible = 0;
 		this.invincible_time = 20.0;
 		this.damage = 10;
+		this.collideDamage = 5;
 		this.team = 0;
 		this.hurt = 0;
 		this.hurt_time = 10.0;
@@ -874,14 +1056,6 @@ var mod_combat = {
 		});
 		
 		this.strike = function(l){
-		/*
-			var offset = new Point(this.flip ? -12 : 12, this.states.attack_down ? 8 : -8 );
-			var range = this.flip ? -8 : 8;
-			var hits = game.overlaps( 
-				new Point( this.position.x + offset.x, this.position.y + offset.y ),
-				new Point( this.position.x + offset.x + range, this.position.y + offset.y + 4 )
-			);
-		*/
 			var offset = new Line( 
 				this.position.add( new Point( l.start.x * (this.flip ? -1.0 : 1.0), l.start.y) ),
 				this.position.add( new Point( l.end.x * (this.flip ? -1.0 : 1.0), l.end.y) )
